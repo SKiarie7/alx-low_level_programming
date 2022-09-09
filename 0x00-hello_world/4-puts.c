54ee708e8c8e7792e0e446bcb17a70d8b17de976
SKiarie7 <k07samuel@gmail.com>
2022-09-08 18:01:12 -0700

    Documentation fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints the required phrase
  * Return: 0 if exited properly, otherwise return a non-zero integer
- */
+ **/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
